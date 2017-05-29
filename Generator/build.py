e80bdae06369a3722429afc400dec95a574a15c3
Erik Campobadal <ConsoleTVs@gmail.com>
2017-05-29 02:02:00 +0200

    Fixes

--- Generator/build.py
+++ Generator/build.py
@@ -4,13 +4,18 @@ import json, glob
 files = glob.glob("*")
 
 dictionary = []
+wc = 0
+output_file = 'output.json'
 
 for file in files:
-    if file != __file__ and file != 'output.json':
+    if file != __file__ and file != output_file:
         f = open(file, 'r', encoding="utf8")
+        print("Generating for language: " + str(file))
         for line in f:
             d = {'language': file, 'word': line.rstrip()}
             dictionary.append(d)
+            wc += 1
 
-output = open('output.json', 'w')
+output = open(output_file, 'w')
 output.write(json.dumps(dictionary, indent=4, sort_keys=True))
+print("Done... " + str(wc) + " total words added in " + str(output_file))
